9cda84cfacdb2fda4e52daaf4ae15bb253dfe0ab
lynn ftmk <alynn412@gmail.com>
2018-08-15 15:27:47 +0800

    fourth commit

--- app/src/main/java/my/edu/utem/myform/MainActivity.java
+++ app/src/main/java/my/edu/utem/myform/MainActivity.java
@@ -5,17 +5,23 @@ import android.content.Intent;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText nameEditText, emailEditText, phoneEditText, cellEditText, messageEditText;
     Button sendButton;
+    Spinner spinner;
+    AutoCompleteTextView autoComp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,18 +33,36 @@ public class MainActivity extends AppCompatActivity {
         cellEditText = findViewById(R.id.main_cellEditText);
         messageEditText = findViewById(R.id.main_messsageEditText);
         sendButton = findViewById(R.id.main_sendButton);
+
+        //(3)set adapter to UI
+        spinner = findViewById(R.id.spinner);  //(1-spinner UI)
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(MainActivity.this,
+                R.array.occupations, android.R.layout.simple_spinner_item); //item dipilih
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);  //set link between adaptar and spinner(UI)
+
+        autoComp = findViewById(R.id.autoComp);
+        ArrayAdapter<CharSequence> adapterAuto = ArrayAdapter.createFromResource(MainActivity.this,
+                R.array.state, android.R.layout.simple_list_item_1);//item dipilih
+        adapterAuto.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        autoComp.setAdapter(adapterAuto);
     }
 
     public void btnPressed(View view) {
         //link to another activity
         Intent intent = new Intent(MainActivity.this, SecondActivity.class);
-        //send together with extra info
-
+                //send together with extra info
         intent.putExtra("name", nameEditText.getText().toString());
         intent.putExtra("email", emailEditText.getText().toString());
         intent.putExtra("phone", phoneEditText.getText().toString());
         intent.putExtra("cell", cellEditText.getText().toString());
         intent.putExtra("message", messageEditText.getText().toString());
+
+        //(4)set display to UI
+        String selectedState = autoComp.getText().toString();
+        String selectedOccupation = (String)(spinner.getSelectedItem());
+        Log.d("debug", "Selected state is " + selectedState);
+        Log.d("debug", "Selected occupation is " + selectedOccupation);
         startActivity(intent);
     }
 
--- app/src/main/java/my/edu/utem/myform/WebActivity.java
+++ app/src/main/java/my/edu/utem/myform/WebActivity.java
@@ -3,14 +3,26 @@ package my.edu.utem.myform;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class WebActivity extends AppCompatActivity {
+
+    WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web);
-
-        Intent intent = getIntent();
+        webView = findViewById(R.id.webView);
+        webView.setWebViewClient(new WebViewClient());
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("http://www.utem.edu.my");
     }
+
+    //public void onBackPressed()
+    //{
+      //  if (webView.canGoBack())
+      //     webView
+    //}
 }
